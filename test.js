a4f0a679b8aba969e064ecbfe870bcd0d19c4660
shinich39 <shinich39@gmail.com>
2024-06-12 00:58:54 +0900

    init

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 import path from "path";
 import fs from "fs";
-import ptd from "./index.js";
+import itd from "./index.js";
 
 const INPUT_PATH = path.join(process.cwd(), "input");
 const OUTPUT_PATH = path.join(process.cwd(), "output");
 
-ptd.exec(INPUT_PATH, OUTPUT_PATH);
+itd.exec(INPUT_PATH, OUTPUT_PATH);
